36b3ab8143c2cc10be2160ef8c835e493d6569c4
unholydeath785 <ivansonofcoul@gmail.com>
2017-07-19 18:47:40 -0700

    created server with express

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const path = require('path');
+const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
+
+const config = require('./config');
+
+const app = express();
+
+//middleware
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
+//Routes
+const RouteManager = require('./routes/RouteManager')(app, config);
+
+app.listen(8080, () => {
+	console.log("Server Listening on port 8080");
+});
